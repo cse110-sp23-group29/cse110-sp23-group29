9f27dddce3b795b13cf95e80a20319a6dd01fd57
aaadit24 <97692709+aaadit24@users.noreply.github.com>
2023-05-27 15:55:35 -0700

    Cards

--- specs/tarotCards/tarotCardsLandingPage.js
+++ specs/tarotCards/tarotCardsLandingPage.js
@@ -1,3 +1,4 @@
+// tarotCardsLandingPage
 function handleVideoClick() {
     let video = document.getElementById("video-player");
     let imageElement = document.getElementById('image');
@@ -8,3 +9,36 @@ function handleVideoClick() {
     });
 }
 
+
+//Card and Flip card and show pop up screen(modal)
+document.addEventListener("DOMContentLoaded", function() {
+    var cards = document.querySelectorAll(".card");
+    var modals = document.querySelectorAll(".modal");
+    var closeModalBtns = document.getElementsByClassName("close");
+
+    // Flip the card and show the modal
+    cards.forEach(function(card, index) {
+      card.addEventListener("click", function() {
+        card.classList.toggle("flipped");
+        modals[index].style.display = "block";
+      });
+    });
+
+    // Close the modal and flip the card back
+    for (var i = 0; i < closeModalBtns.length; i++) {
+      closeModalBtns[i].addEventListener("click", function() {
+        var modal = this.parentElement.parentElement;
+        modal.style.display = "none";
+        var index = Array.from(modals).indexOf(modal);
+        cards[index].classList.remove("flipped");
+      });
+    }
+
+    window.addEventListener("click", function(event) {
+      if (event.target.classList.contains("modal")) {
+        event.target.style.display = "none";
+        var index = Array.from(modals).indexOf(event.target);
+        cards[index].classList.remove("flipped");
+      }
+    });
+  });
