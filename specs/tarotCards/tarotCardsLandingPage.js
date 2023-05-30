e37898d5270d0b6f305ec171563e1a293481b4d9
noahdj37 <noahdj37@gmail.com>
2023-05-30 14:35:27 -0700

    Re-align CodeTeam2 branch with main
    fixing CodeTeam2 branch in preparation of merge

--- specs/tarotCards/tarotCardsLandingPage.js
+++ specs/tarotCards/tarotCardsLandingPage.js
@@ -1,50 +1,9 @@
-// tarotCardsLandingPage
 function handleVideoClick() {
-    let video = document.getElementById("video-player");
-    let imageElement = document.getElementById('image');
-    video.play();
-    video.addEventListener('ended', function() {
-        video.style.display = 'none';
-        imageElement.src = '../specs/images/ComputerScreenDefault.png';
-        showCards();
-    });
-}
-function showCards() {
-  var cards = document.querySelectorAll(".card");
-  cards.forEach(function(card) {
-    card.style.display = "block";
+  let video = document.getElementById("video-player");
+  let imageElement = document.getElementById('image');
+  video.play();
+  video.addEventListener('ended', function() {
+      video.style.display = 'none';
+      imageElement.src = '../specs/images/ComputerScreenDefault.png';
   });
 }
-
-//Card and Flip card and show pop up screen(modal)
-document.addEventListener("DOMContentLoaded", function() {
-    var cards = document.querySelectorAll(".card");
-    var modals = document.querySelectorAll(".modal");
-    var closeModalBtns = document.getElementsByClassName("close");
-
-    // Flip the card and show the modal
-    cards.forEach(function(card, index) {
-      card.addEventListener("click", function() {
-        card.classList.toggle("flipped");
-        modals[index].style.display = "block";
-      });
-    });
-
-    // Close the modal and flip the card back
-    for (var i = 0; i < closeModalBtns.length; i++) {
-      closeModalBtns[i].addEventListener("click", function() {
-        var modal = this.parentElement.parentElement;
-        modal.style.display = "none";
-        var index = Array.from(modals).indexOf(modal);
-        cards[index].classList.remove("flipped");
-      });
-    }
-
-    window.addEventListener("click", function(event) {
-      if (event.target.classList.contains("modal")) {
-        event.target.style.display = "none";
-        var index = Array.from(modals).indexOf(event.target);
-        cards[index].classList.remove("flipped");
-      }
-    });
-  });
