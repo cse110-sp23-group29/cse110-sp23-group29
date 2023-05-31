8123b50201015cc950daaaefb635934c128ec921
noahdj37 <noahdj37@gmail.com>
2023-05-30 21:24:50 -0700

    Cypress Sprint 1 Test Complete
    test reaching website, opening transition, and clicking and exiting all 3 cards

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,33 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Push Spec', () => {
+  it('Main', () => {
+    //Visit landing page
+    cy.visit('https://cse110-sp23-group29.github.io/')
+
+    //Click start
+    cy.get('video')
+      .should('have.prop', 'paused', true)
+      .and('have.prop', 'ended', false)
+      .click()
+
+    //Verify video is played and finishes
+    cy.get('video')
+      .should('have.prop', 'paused', false)
+      .and('have.prop', 'ended', false)
+
+    //Check page has switched to main card page
+    cy.url({timeout: 10000})
+      .should('eq', 'https://cse110-sp23-group29.github.io/BlueScreen.html')
+
+    //Click each card
+    cy.get('.card')
+      .should('have.length', 3)
+      .each(($card, index, $list) => {
+        cy.wrap($card).click()
+
+        cy.get('.close')
+          .then(($close) => {
+            $close[index].click()
+          })
+      })
   })
 })
