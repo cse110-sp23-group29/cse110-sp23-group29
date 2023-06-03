48c45e2ed3cc8dc8fac6e14c9993140d0bdf8b9f
MelodyyyYin <melodyyin31@gmail.com>
2023-06-03 15:56:09 -0700

    darkmode update

--- specs/tarotCards/StartScreen.js
+++ specs/tarotCards/StartScreen.js
@@ -42,3 +42,40 @@ function openPopup() {
 function closePopup() {
     document.getElementById("popup").style.display = "none";
 }
+function toggleDarkMode(checkbox) {
+    const body = document.querySelector('body');
+    // body.classList.toggle('dark-mode');
+    if(checkbox.checked){
+        body.classList.toggle("dark-class");
+    } else {
+        body.classList.remove("dark-class");
+    }
+    // var darkModeCheckbox = document.getElementById("dark-mode-checkbox");
+    // var isDarkMode = darkModeCheckbox.checked;
+  
+    // // Store the dark mode state in Local Storage
+    // localStorage.setItem("darkMode", isDarkMode);
+}
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Retrieve the dark mode state from Local Storage
+//     var isDarkMode = localStorage.getItem("darkMode");
+    
+//     // Apply the dark mode state
+//     var darkModeCheckbox = document.getElementById("dark-mode-checkbox");
+//     darkModeCheckbox.checked = isDarkMode === "true";
+    
+//     // Apply the appropriate theme based on the dark mode state
+//     applyTheme(isDarkMode);
+//   });
+
+// function applyTheme(isDarkMode) {
+//     if (isDarkMode) {
+//         document.body.classList.add("dark-class");
+//     } else {
+//         document.body.classList.remove("dark-class");
+//     }
+// }
+
+// const darkModeCheckbox = document.querySelector('#dark-mode-checkbox');
+// darkModeCheckbox.addEventListener('change', toggleDarkMode);
+  
