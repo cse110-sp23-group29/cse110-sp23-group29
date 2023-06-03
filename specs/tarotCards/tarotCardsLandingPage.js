613a284dadd84e6803cc6b15c0594f516d6d27bd
Simone <gaminggyrrl@gmail.com>
2023-06-03 14:29:24 -0700

    Fixed visuals and added a transition animation.

--- specs/tarotCards/tarotCardsLandingPage.js
+++ specs/tarotCards/tarotCardsLandingPage.js
@@ -1,7 +1,27 @@
 function handleVideoClick() {
   let video = document.getElementById("video-player");
-  let imageElement = document.getElementById('image');
+  let transitionVideo = document.getElementById("fade-in-video");
+  let computer = document.getElementsByClassName("image-container")[0];
+
+  // zoom in on computer if window is bigger than 700px.
+  // if window is any smaller, the zoom in wont scale right.. will fix later
+  if (window.innerWidth > 700) {
+    let rangeX = computer.offsetWidth;
+    let vw = 0;
+    vw = rangeX / Math.min(window.innerWidth, window.innerHeight);
+    document.documentElement.style.setProperty('--vw-scale', `${vw}`);
+    
+    computer.style.transition = "all 600ms ease-in";
+    computer.style.transform = "translateY(11%) scale(var(--vw-scale))";
+  }
+  
   video.play();
+  transitionVideo.play();
+
+  setTimeout(function(){
+    transitionVideo.classList.add("fadeInAndOut");
+  }, 1500);
+
   video.addEventListener('ended', function() {
       window.location.href = '../StartScreen.html'
   });
