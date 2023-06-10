42b540ee08a83c1f443f4724ac6efb3246695302
noahdj37 <noahdj37@gmail.com>
2023-06-09 17:28:47 -0700

    Add load listener to BlueScreen

--- specs/tarotCards/BlueScreen.js
+++ specs/tarotCards/BlueScreen.js
@@ -1,5 +1,34 @@
 const audioSettings = new AudioSettings('bg-music', 'music-button', 'volume-slider', 'volume-icon');
 
+document.addEventListener('load', () => {
+  // Apply settings
+  audioSettings.volumeSlider.addEventListener("input", (e) => {
+    audioSettings.adjustVolume(e.currentTarget.value / 100);
+  });
+
+  audioSettings.musicCheckBox.addEventListener('change', () => {
+      audioSettings.toggleMusic();
+  });
+
+  audioSettings.volumeIcon.addEventListener('click', () =>{
+      audioSettings.toggleVolumeIcon();
+  });
+
+  // if audio should be paused, pause and change volume icon
+  if (localStorage.getItem("paused")) {
+      audioSettings.audio.pause();
+      audioSettings.volumeIcon.src = "./specs/images/volume-off.png";
+  }
+  else {
+      audioSettings.audio.play();
+      audioSettings.volumeIcon.src = "./specs/images/volume-on.png";
+  }
+
+  // if the audio is paused but the music checkbox is checked, uncheck it
+  if (audioSettings.audio.paused && audioSettings.musicCheckBox.checked) 
+      audioSettings.musicCheckBox.click();
+});
+
 // Declaring the selectedArcana object
 var selectedArcana = {
     past: [],
