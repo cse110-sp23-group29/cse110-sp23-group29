91bbc1f4f14f1741a13ad7ab8fa98b5e8479d132
noahdj37 <noahdj37@gmail.com>
2023-06-09 17:06:57 -0700

    Fix StartScreen merge

--- specs/tarotCards/StartScreen.js
+++ specs/tarotCards/StartScreen.js
@@ -20,46 +20,14 @@ window.addEventListener("load", function() {
         audioSettings.audio.pause();
         audioSettings.volumeIcon.src = "./specs/images/volume-off.png";
     }
-    localStorage.setItem("paused", audio.paused);
-});
+    else {
+        audioSettings.audio.play();
+        audioSettings.volumeIcon.src = "./specs/images/volume-on.png";
+    }
 
-const volumeSlider = document.getElementById('volume-slider');
-audio.volume = volumeSlider.value / 100;
-
-localStorage.setItem("volume", volumeSlider.value / 100);
-localStorage.setItem("paused", audio.paused);
-
-volumeSlider.addEventListener("input", (e) => {
-    audio.volume = e.currentTarget.value / 100;
-    localStorage.setItem("volume", audio.volume);
-});
-
- function playMusic(checkbox){
-     if(checkbox.checked){
-         audio.play();
-     } else {
-         audio.pause();
-     }
-     localStorage.setItem("paused", audio.paused);
- }
-
- var isAudioPlaying = true;
-
- function toggleVolume() {
-     var volumeIcon = document.getElementById("volumeIcon");
-    
-     if (isAudioPlaying) {
-         audio.pause();
-         volumeIcon.src = "./specs/images/volume-off.png";
-         isAudioPlaying = false;
-         musicButton.checked = false; 
-     } else {
-         audio.play();
-         volumeIcon.src = "./specs/images/volume-on.png";
-         isAudioPlaying = true;
-         musicButton.checked = true; 
-     }
- }
+    // if the audio is paused but the music checkbox is checked, uncheck it
+    if (audioSettings.audio.paused && audioSettings.musicCheckBox.checked) 
+        audioSettings.musicCheckBox.click();
 
     if (localStorage.getItem("darkMode") === 'true')
         document.body.classList.add("dark-class");
