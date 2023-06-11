922afb5910dfe6e58e48abbe780d15eda8ba1ef2
jubamadu <jubamadu@ucsd.edu>
2023-06-10 18:28:03 -0700

    JSDoc Further Edit #5

--- documentation/MarkdownDocumentation.md
+++ documentation/MarkdownDocumentation.md
@@ -1,2 +1,3 @@
+## Documentation Link
 
-[JS Documentation] (cse110-sp23-group29.github.io/documentation/index.html)
+[JS Documentation](cse110-sp23-group29.github.io/documentation/index.html)
--- specs/tarotCards/testJSFile.js
+++ specs/tarotCards/testJSFile.js
@@ -14,3 +14,12 @@ function exampleJSComment(){
 function exampleFunctionSecond(test){
 
 }
+/**
+* Handle Video Click
+* @param {string} param1 test Paramater
+* @param {string} param2 test Paramater
+* @returns {string}None
+*/
+function exampleFunctionThird(param1,param2){
+
+}
